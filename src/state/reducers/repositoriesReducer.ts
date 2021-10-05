d13e184f68b880904709c0b08b9ec4d2ed9666ce
maxts0gt <bazarsadtsogtjargal@gmail.com>
2021-10-06 01:36:59 +0900

    initial commit

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,29 @@
+import { ActionType } from '../action-types';
+import { Action } from '../action';
+
+interface RepositoriesState {
+	loading: boolean;
+	error: string | null;
+	data: string[];
+}
+
+const initialState = {
+	loading: false,
+	error: null,
+	data: [],
+};
+
+const reducer = (state: RepositoriesState = initialState, action: Action): RepositoriesState => {
+	switch (action.type) {
+		case ActionType.SEARCH_REPOSITORIES:
+			return { loading: true, error: null, data: [] };
+		case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+			return { loading: false, error: null, data: action.payload };
+		case ActionType.SEARCH_REPOSITORIES_ERROR:
+			return { loading: false, error: null, data: [] };
+		default:
+			return state;
+	}
+};
+
+export default reducer;
